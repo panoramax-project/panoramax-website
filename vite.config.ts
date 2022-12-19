df8bc275e19f8a2fafaf44ec0ee356008261e9b7
Andreani Jean <ja.andreani@gmail.com>
2022-12-19 15:29:05 +0100

    change base url to deploy git push

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/panoramax-website/',
   plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: {
