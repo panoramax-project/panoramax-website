1b94b8d716969975216a5d7305eabad86c043ae2
Andreani Jean <ja.andreani@gmail.com>
2023-03-29 15:18:06 +0200

    remove docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/panoramax-website/docs/',
+  base: '/',
   plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: {
